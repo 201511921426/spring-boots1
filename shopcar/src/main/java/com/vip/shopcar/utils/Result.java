2190f8d354d7aa9e74fb5d2dc78595e09b9cad4c
pcl <407091625@qq.com>
2019-09-19 18:04:39 +0800

    test

---
+++ shopcar/src/main/java/com/vip/shopcar/utils/Result.java
@@ -0,0 +1,26 @@
+package com.vip.shopcar.utils;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    private String msg;
+    private int status;
+    private T data;
+
+    public static Result success(Object obj) {
+        Result result = new Result();
+        result.setStatus(config.SUCCESS_STATUS_200);
+        result.setData(obj);
+        result.setMsg(config.SUCCESS_MSG);
+        return result;
+    }
+
+
+    public static Result error() {
+        Result result = new Result();
+        result.setStatus(config.ERROR_STATUS_404);
+        result.setMsg(config.ERROR_MSG);
+        return result;
+    }
+}
---
+++ shopcar/src/main/java/com/vip/shopcar/utils/config.java
@@ -0,0 +1,12 @@
+package com.vip.shopcar.utils;
+
+import lombok.Data;
+
+@Data
+public class config {
+    public static final String SUCCESS_MSG = "success";
+    public static final String ERROR_MSG = "error";
+    public static final int SUCCESS_STATUS_200 = 200;
+    public static final int ERROR_STATUS_404 = 404;
+
+}
