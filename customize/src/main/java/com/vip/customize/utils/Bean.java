730fd196a5e0e61c478f16de9d81193999a6f06a
wangdaye <535332342@qq.com>
2019-09-19 15:25:15 +0800

    基础工程

---
+++ customize/src/main/java/com/vip/customize/utils/Bean.java
@@ -0,0 +1,12 @@
+package com.vip.customize.utils;
+
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class Bean implements Serializable {
+
+}
---
+++ web-about/src/main/java/com/vip/webabout/WebAboutApplication.java
@@ -0,0 +1,13 @@
+package com.vip.webabout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAboutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAboutApplication.class, args);
+    }
+
+}
